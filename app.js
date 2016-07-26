93f0a37744807294f842619cc61eea0a3fb3d349
Yak Jun Xiang <snowypowers@gmail.com>
2016-07-26 22:08:50 +0800

    upgrade jquery to v3.1

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 //Static folder for all assets
 app.use(express.static('public'));
 
+app.get('/jquery/jquery.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+});
+
 app.get('/', function (req, res) {
     res.sendFile('/views/index.html', {root: __dirname});
 });
@@ -11,3 +15,4 @@ app.get('/', function (req, res) {
 app.listen(3000, function() {
     console.log("Listening at port 3000");
 });
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 // Load is used to ensure all images have been loaded, impossible with document
-
-jQuery(window).load(function () {
+$(window).on('load', function () {
 
 
 
 	// Takes the gutter width from the bottom margin of .post
 
-	var gutter = parseInt(jQuery('.post').css('marginBottom'));
-	var container = jQuery('#posts');
+	var gutter = parseInt($('.post').css('marginBottom'));
+	var container = $('#posts');
+	console.log(container);
 
 
 
@@ -24,25 +24,25 @@ jQuery(window).load(function () {
 	// This code fires every time a user resizes the screen and only affects .post elements
 	// whose parent class isn't .container. Triggers resize first so nothing looks weird.
 	
-	jQuery(window).bind('resize', function () {
-		if (!jQuery('#posts').parent().hasClass('container')) {
+	$(window).bind('resize', function () {
+		if (!$('#posts').parent().hasClass('container')) {
 			
 			
 			
 			// Resets all widths to 'auto' to sterilize calculations
 			
-			post_width = jQuery('.post').width() + gutter;
-			jQuery('#posts, body > #grid').css('width', 'auto');
+			post_width = $('.post').width() + gutter;
+			$('#posts, body > #grid').css('width', 'auto');
 			
 			
 			
 			// Calculates how many .post elements will actually fit per row. Could this code be cleaner?
 			
-			posts_per_row = jQuery('#posts').innerWidth() / post_width;
+			posts_per_row = $('#posts').innerWidth() / post_width;
 			floor_posts_width = (Math.floor(posts_per_row) * post_width) - gutter;
 			ceil_posts_width = (Math.ceil(posts_per_row) * post_width) - gutter;
-			posts_width = (ceil_posts_width > jQuery('#posts').innerWidth()) ? floor_posts_width : ceil_posts_width;
-			if (posts_width == jQuery('.post').width()) {
+			posts_width = (ceil_posts_width > $('#posts').innerWidth()) ? floor_posts_width : ceil_posts_width;
+			if (posts_width == $('.post').width()) {
 				posts_width = '100%';
 			}
 			
@@ -50,8 +50,8 @@ jQuery(window).load(function () {
 			
 			// Ensures that all top-level elements have equal width and stay centered
 			
-			jQuery('#posts, #grid').css('width', posts_width);
-			jQuery('#grid').css({'margin': '0 auto'});
+			$('#posts, #grid').css('width', posts_width);
+			$('#grid').css({'margin': '0 auto'});
         		
 		
 		
