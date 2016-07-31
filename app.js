30281a0ded2c75aa977f7a0baa259fff413900ea
Hazel Lau <Hazel Lau>
2016-07-31 20:54:32 +0800

    Installed pug and tested

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 
 //Static folder for all assets
 app.use(express.static('public'));
+app.set('view engine', 'pug'); 
 
 app.get('/jquery/jquery.js', function(req, res) {
     res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
@@ -11,6 +12,10 @@ app.get('/jquery/jquery.js', function(req, res) {
 app.get('/', function (req, res) {
     res.sendFile('/views/index.html', {root: __dirname});
 });
+
+app.get("/test", function (req, res) {
+	res.render('test', {title:'YOHO', message:'MEOWLALA'});
+}); 
 
 app.listen(3000, function() {
     console.log("Listening at port 3000");
