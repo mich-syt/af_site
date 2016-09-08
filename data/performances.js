90dd1a0466edd24f8623178956be3beb1454aba2
Hazel Lau <Hazel Lau>
2016-09-08 13:33:09 +0800

    added arts market page photos

--- data/performances.js
+++ data/performances.js
@@ -1,101 +1,115 @@
 var data = [
 	{
 		performer: "Drama Society", 
-		title: "TBC", 
-		description: "TBC", 
-		venue: "TBC", 
-		timing: "TBC"
+		title: "Production by Drama Society", 
+		description: "Coming soon!", 
+		venue: "Campus Center", 
+		timing: "14th October 5.30pm", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	}, 
 	{
 		performer: "Pinardy Yang", 
 		title: "Piano Performance", 
 		description: "Having a background in classical piano, Pinardy Yang was engaged in playing music from young. Joining this year’s Arts Fest as a performer, he hopes to spread some classical music around while sharing the joy of music through performance. \n Pinardy will be performing Toccato in E Minor, BWV 914 by JS Bach and Intermezzo, Op. 119 No.2 by Johannes Brahms.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/pinardy01.jpg"]
 	},
 	{
 		performer: "Bing Han Quek", 
 		title: "Vocals/Guitar Performance", 
 		description: "", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Tan Jun Qi and Joshia Seam", 
 		title: "Vocals/Piano Performance", 
 		description: "Unwind with the duo as they chill out to One Summer’s Day from Spirited Away and Princess Mononoke Theme from the titular Studio Ghibli movie. \n\n Jun Qi’s first public performance was during Freshnomore, when she performed a Japanese song on the keyboard with vocals. Since then, she has enjoyed arranging piano accompaniments and singing various Japanese songs, deciding to push the boundaries of both her singing and arrangement skills by choosing two harmonically complex and vocally challenging pieces by Joe Hisaishi. Through the performance, Jun Qi aims to convey the soulful beauty of the two pieces. \n Joshia picked up piano when he was 4, and since then was awarded the young performer award while studying at ACJC MEP. ", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Ashwin Venkatram", 
 		title: "Carnatic Violin Duet", 
 		description: "Ashwin will be opening with Vatapi Ganapatim, a Sanskrit song by Muthuswami Dikshitar (1775–1835), one of the \"Trinity of Carnatic music\" and a hymn traditionally sung at the beginning of many Carnatic music concerts, followed by English Note - a music piece that borrows attributes from both Indian Classical music as well as Western music to create a fast paced song.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Tandav", 
 		title: "Bolly Rock and Pop", 
 		description: "Formed by band members Shobhit (guitarist), Vishal (keyboardist), Loo Bin (drummer) and Akash (vocals), Tandav is known on stage for their lively take on Indian Bolly music. Featuring songs like Shanti Mantra (metallic version), you’ll soon find yourself head-banging along to the rest of the lineup.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Wushu Club", 
 		title: "Martial Arts Performance", 
 		description: "Since June 2015, the SUTD Wushu club has continued to wow the audience with their finesse and performance. As much as it is a combat sport, Wushu is also often practiced for exhibition displays, both demanding skilled coordination to execute each moveset.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Taekwondo Club", 
 		title: "Martial Arts Performance", 
 		description: "Taekwondo consists of three unique elements - Tae (태) stands for leg, Kwon (권) for fist, and Do (도) for discipline. Since its conception in September 2013, SUTKD has made it their mission to create a sustainable culture of Taekwondo in SUTD.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},
 	{
 		performer: "Vertex", 
 		title: "Cheer Performance", 
 		description: "Cheering since 2012, Vertex came together from a collective love and interest in cheerleading. With their contagious energy and eye-grabbing routine, Vertex is sure to leave you hyped up for the rest of the evening.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},	
 	{
 		performer: "Vocomotives", 
 		title: "A Cappella Performance", 
 		description: "SUTD Vocomotives was formed in 2012 by the first batch of SUTD students with a strong passion for A Cappella music. The name Vocomotives came from the word “Locomotion”, characterizing their ever-changing nature, consistently improving themselves along the way.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},	
 	{
 		performer: "Taal", 
 		title: "Indian Dance Performance", 
 		description: "When Indian Dance reestablished themselves as Taal, meaning “beat”, they returned along with a renewed energy to spread the joy of dancing and Indian culture through its various dance forms. Holding the belief that Taal is for anyone who is passionate about dance or simply wants to be part of the family, they have grown significantly in their learning and sharing of passion in the short time that they created an identity for themselves.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},	
 	{
 		performer: "Indonesian Cultural Club", 
 		title: "Saman Dance", 
 		description: "Also called ‘the dance of thousand hands’, the Saman dance is one of the most practiced among Indonesian traditional dances. Characterized by its fast-paced rhythm and common harmony between dancers, these two elements are key figures of Saman, along with its notable disuse of music instruments. Audience members are invited to clap along to the dance, joining the performance from the crowd.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},	
 	{
 		performer: "Michael Sebastian", 
 		title: "Magic Show", 
 		description: "Michael has been practicing magic since a very young age. Born and raised in Indonesia, where magicians were often as popular as actors and musicians, he was exposed to magic early and learned his first trick from his uncle. He was captivated by its power to challenge reason, perception, and reality. Now, he continues to practice magic as a hobby to remind himself to always have the correct perspective and to live out his life motto: \"Nothing is impossible.\"", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	},	
 	{
 		performer: "BANDS", 
 		title: "Various", 
 		description: "Year after year, SUTD Bands has been responsible for organizing largely-attended events such as the Open Mic Sessions series and the recent Summer Vibes concert. Formed as a collective of student individual performers and bands, Bands continues to provide platforms for young talents on campus to showcase their music as one of the biggest supporters of our performing arts community.", 
 		venue: "TBC", 
-		timing: "TBC"
+		timing: "TBC", 
+		imgs: ["/img/performers/dramatemporary.jpg"]
 	}
 ];
 
