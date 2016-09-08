6a201428c5b7be02657f5a0825853f13059aeeff
Hazel Lau <Hazel Lau>
2016-09-08 23:51:04 +0800

    updated writeups of booths

--- data/arts_market.js
+++ data/arts_market.js
@@ -2,34 +2,73 @@ var data = [
 	{
 		vendorname: "Yun Jie",
 		boothname: "Illustration Prints",
-		description: "Hello! \n Yun Jie here. I’ll be one of the artists having a booth at the Art Market :) I would be selling A5 prints of my illustrations. They are really just these happy little stick people with inspirational quotes that go with them. I gain inspiration from quotes, lyrics and some are based off my own experiences. Each illustration has a story behind them and if you would like to know more about these stories or if you are looking for cards to give your friends, do drop by at the Art Market! Hope to see you guys there :)",
-		openinghours: "Coming soon!",
+		description: "Hello! <br> Yun Jie here. I’ll be one of the artists having a booth at the Art Market :) I would be selling A5 prints of my illustrations. They are really just these happy little stick people with inspirational quotes that go with them. I gain inspiration from quotes, lyrics and some are based off my own experiences. Each illustration has a story behind them and if you would like to know more about these stories or if you are looking for cards to give your friends, do drop by at the Art Market! Hope to see you guys there :)",
+		venue: "Campus Center",
+		openinghours: "October 14th, 2-7.30pm",
 		link: "https://www.instagram.com/yj.art/",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
+	}, 
+	{
+		vendorname: "Samantha Lim",
+		boothname: "TheSamSignature",
+		description: "<strong>Miniature Clay Figurines</strong> <br>I first experimented with polymer clay about 2 years ago, and some may know me from the clay workshop in SUTD Makerfair 2016. Majority of my miniatures are gifts to people, and hence most of them are not on display. However, you can still find the pictures on my instagram. If my little creations have captured your attention, please buy them :) if you would like to order instead, feel free to contact me through the various ways below. Thank you!",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm <br>October 14th, 2-7.30pm",
+		link: "https://www.instagram.com/thesamsignature/ | https://carousell.com/thesamsignature",
+		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
+	}, 
+	{
+		vendorname: "SUTD Diving Club",
+		boothname: "Photobooth and Diving Bags",
+		description: "We are a group of diving enthusiasts from SUTD who dive frequently around Asia and beyond. Come by our booth for a chance to don up some diving gear and snap some photos, we will be selling dry bags too!",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm <br>October 14th, 2-7.30pm",
+		link: "https://www.facebook.com/groups/SUTDdiving",
+		imgs: ["/img/boothspage/divingclub.jpg"]
+	}, 
+	{
+		vendorname: "SUTD Photography Circle",
+		boothname: "Postcards",
+		description: "Made by our very own SUTD photographers, each postcard is a unique piece of work by various members of Photog Circle.",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm",
+		link: "https://www.facebook.com/groups/sutdphotogcircle",
+		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
+	}, 
+	{
+		vendorname: "SUTD Drama Society",
+		boothname: "Live Sculpturing",
+		description: "\"Sculpt me like one of your French girls.\" <br>Drama Society will be holding live human sculpturing activities at various locations. Feel free to carry the living statues around, or position them however you like.",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm",
+		link: "",
+		imgs: ["/img/boothspage/dramasociety.jpg"]
+	}, 
+	{
+		vendorname: "SUTD E-Sports",
+		boothname: "Live Portrait Drawing",
+		description: "SUTD E-Sports will be inviting down an artist from Razor Inc. to draw live portraits of students.",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm",
+		link: "https://www.facebook.com/Sutd.ESports/",
+		imgs: ["/img/boothspage/ESports.png"]
 	}, 
 	{
 		vendorname: "Rotaract Club",
 		boothname: "拉lipop & Polaroid Photo Booth",
 		description: "Test your luck at Rotaract's 拉lipop Booth! Stand to win attractive notebooks when you pull out the correct lollipops. Rotaract is also organising a polaroid photo booth too! Come try your luck and leave unforgettable memories at Rotaract's booth on Friday. See you there!",
-		openinghours: "Coming soon!",
+		venue: "Campus Center",		
+		openinghours: "October 12th, 7-10pm",
 		link: "",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 		
 	}, 
 	{
-		vendorname: "Any Chopra",
-		boothname: "Vegan Food",
-		description: "",
-		openinghours: "Coming soon!",
-		link: "",
-		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
-
-	}, 
-	{
 		vendorname: "Namaste Club",
 		boothname: "Henna Tattoos",
 		description: "Get henna tattoos from our very own SUTD henna artists! If our expert artists are around, they can even draw any design (e.g. cartoons, your favourite band logo, etc.) that you want! Hope to see you at our booth!",
-		openinghours: "Coming soon!",
+		venue: "Campus Center",		
+		openinghours: "October 10th, 6-8pm",
 		link: "",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -38,7 +77,8 @@ var data = [
 		vendorname: "Yi Yang",
 		boothname: "Tote Bag & Automata DIY",
 		description: "You know you want to buy a tote bag with a nice tree drawing on it.That and a wooden automation toy. Email me at tengyiyang@gmail.com. I don't bite.",
-		openinghours: "Coming soon!",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-8pm",
 		link: "https://www.instagram.com/teewhywhy/",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -46,25 +86,28 @@ var data = [
 	{
 		vendorname: "French Connection",
 		boothname: "Mason Jar DIY",
-		description: "Every wondered what kind of an artist you are? Come join us at our Jar factory and find out! Just grab a jar at our booth, personalise your jar any way you like, weigh your jar and pay! Simple and easy. We have lots of random scrap materials for you to french things up. \n Also, approach us and get to learn techniques like decoupage, decal transfer and many more! Hint hint, it makes for a great gift for your friends and loved ones ;)",
-		openinghours: "Coming soon!",
+		description: "Every wondered what kind of an artist you are? Come join us at our Jar factory and find out! Just grab a jar at our booth, personalise your jar any way you like, weigh your jar and pay! Simple and easy. We have lots of random scrap materials for you to french things up. <br> Also, approach us and get to learn techniques like decoupage, decal transfer and many more! Hint hint, it makes for a great gift for your friends and loved ones ;)",
+		venue: "Campus Center",		
+		openinghours: "October 12th, 7-10pm <br>October 14th, 2-7.30pm",
 		link: "",
-		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
+		imgs: ["/img/boothspage/frenchclub01.jpg", "/img/boothspage/frenchclub02.jpg", "/img/boothspage/frenchclub03.jpg"]
 
 	}, 
 	{
 		vendorname: "Vocomotives",
 		boothname: "Music Booth",
-		description: "Ever wondered how singers harmonize like they do in Pitch Perfect? Vocamotives will be holding a teaching booth on basic A Cappella and beatboxing techniques.",
-		openinghours: "Coming soon!",
+		description: "Ever wondered how singers harmonize like they do in Pitch Perfect? Vocomotives will be holding a teaching booth on basic A Cappella and beatboxing techniques.",
+		venue: "Campus Center",		
+		openinghours: "October 12th, 7-10pm <br>October14th, 2-7.30pm",
 		link: "",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 	}, 
 	{
 		vendorname: "Yun En",
 		boothname: "Cloud on Canvas",
-		description: "Cloud On Canvas is the brainchild of of a aspiring artist, who believes that all things are made better through good design. Besides handlettered and hand-drawn prints, customized calligraphy services are also available for those seeking something a little more unique. She hopes that her creations are able to speak to people and make their bad days good, their good days better.",
-		openinghours: "Coming soon!",
+		description: "<strong>Handlettered Cards and Stickers </strong><br>Cloud On Canvas is the brainchild of of a aspiring artist, who believes that all things are made better through good design. Besides handlettered and hand-drawn prints, customized calligraphy services are also available for those seeking something a little more unique. She hopes that her creations are able to speak to people and make their bad days good, their good days better.",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm",
 		link: "https://www.instagram.com/yunennn",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -73,7 +116,8 @@ var data = [
 		vendorname: "Zann Koh",
 		boothname: "Crochet Art",
 		description: "Turning one dimension into three - '3d-printing' with yarn! That's what crochet does. Do come by to see a few examples of cute crochet art, as well as preorder your own!",
-		openinghours: "Coming soon!",
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm",
 		link: "",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -81,8 +125,8 @@ var data = [
 	{
 		vendorname: "Nabila Pranoto",
 		boothname: "NLPDesigns",
-		description: "'Fashions fade, style is eternal.' —Yves Saint Laurent \n Art and Design for the Fashionably Bold: started by Nabila, NLPDesigns is a Singapore-based store specializing in handpainted customised leather bags. Immortalise your style in our very own custom leather bags, personally handpainted and designed only for you.",
-		openinghours: "coming soon!",
+		description: "<strong>Handpainted Leather Bags</strong><br>'Fashions fade, style is eternal.' —Yves Saint Laurent <br>Art and Design for the Fashionably Bold: started by Nabila, NLPDesigns is a Singapore-based store specializing in handpainted customised leather bags. Immortalise your style in our very own custom leather bags, personally handpainted and designed only for you.",
+		openinghours: "Coming soon!",
 		link: "https://www.instagram.com/nlpdesigns | https://www.facebook.com/NLPDesigns",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -91,7 +135,8 @@ var data = [
 		vendorname: "Multiverse", 
 		boothname: "Zines and Prints Booth", 
 		description: "Selling stickers, our first print issue and an interactive area either for zine-making or a portrait wall.",
-		openinghours: "Coming soon!", 
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm", 
 		link: "https://www.instagram.com/multiversemag | https://www.facebook.com/multiversemag",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
@@ -99,8 +144,9 @@ var data = [
 	{
 		vendorname: "SUTDAF", 
 		boothname: "Pillar+", 
-		description: "Pillar+ is an Arts Fest initiative aimed at promoting our student community by providing a platform for various individuals and fifth rows to showcase and sell their peripherals. All proceeds will go towards the supporting of the annual event and its affiliated fifth rows. \n With a focus on creativity and customization, students are encouraged to send in their own designs - which will be each personally produced by the team at Arts Fest - for collection during event week, or come down to make your own handmade designs at our booth. Either way, it’s a great little souvenir - something unique to take home along with your memories of Arts Fest.", 
-		openinghours: "Coming soon!", 
+		description: "<strong>Keychain DIY and Store Collection Booth</strong> <br>Pillar+ is an Arts Fest initiative aimed at promoting our student community by providing a platform for various individuals and fifth rows to showcase and sell their peripherals. All proceeds will go towards the supporting of the annual event and its affiliated fifth rows. <br> With a focus on creativity and customization, students are encouraged to send in their own designs - which will be each personally produced by the team at Arts Fest - for collection during event week, or come down to make your own handmade designs at our booth. Either way, it’s a great little souvenir - something unique to take home along with your memories of Arts Fest.", 
+		venue: "Campus Center",
+		openinghours: "October 10th, 6-10pm <br>October 12th, 7-10pm <br>October 14th, 2-7.30pm", 
 		link: "https://www.instagram.com/im_possibilities | https://www.facebook.com/SUTDArtsFest",
 		imgs: ["https://placekitten.com/500/400", "https://placekitten.com/500/400", "https://placekitten.com/500/400"]
 
