11832e21dc4a91e468e84c57e0fd4fcbb46698bf
Hazel Lau <Hazel Lau>
2016-10-03 20:32:38 +0800

    Changes to schedule for workshop

--- data/workshops.js
+++ data/workshops.js
@@ -3,9 +3,9 @@ module.exports = {
         title: "Crocheting",
         splash: "/img/headers/Workshop_Crocheting.jpg",
 		description: "Crocheting is a wonderful way of relieving stress, while its versatility allows for it to be done whenever and wherever. Learn how to knit something nifty while waiting for the bus! This workshop will not only teach the basics of the trade for all starting members, but participants will also be engaged in a two-session project where you will be able to make your own mobile phone pouch. After all, practice makes good patterns.",
-		venue: "Think Tank TBC", 
+		venue: "Think Tank 18", 
 		timing: "October 10th, 7.30-10pm", 
-		venue2: "Think Tank TBC", 
+		venue2: "Think Tank 18", 
 		timing2: "October 14th, 2.30-5pm",
 		fee: "7 (includes materials and instructions)", 
 		size: 20 
@@ -26,7 +26,6 @@ module.exports = {
 		description: "This workshop provides a brief, hands-on experience in using microbes as a medium for artworks. Participants are guided to design (hand-drawn or digital) original patterns, then trace them onto a Petri dish using a harmless strain of E. coli in a solution. Left overnight in an incubator, the bacteria grows and the pattern takes shape on the next day, by which time they will be displayed at the workshop. Now you can art with science!",
 		venue: "Biology Learning Lab",
 		timing: "October 12th & 14th, 3-5pm", 
-		fee: "TBC",
 		size: 10
 	},
 	
@@ -34,7 +33,7 @@ module.exports = {
 		title: "Spoken Word and Poetry",
 		splash: "/img/headers/Workshop_SpokenWord.jpg",
 		description: "Spoken Word is a performance incorporating poetry, comedy and drama all in one. Many people scoff at poetry from being too confounding, comedy for being too much of a joke, drama for being plain dramatic. Spoken word is none of those. In fact, almost everyone has done it. Join us for the Spoken Word Poetry Workshop and attain the key to unleashing your spoken word.", 
-		venue: "Think Tank TBC",
+		venue: "Think Tank 17",
 		timing: "October 14th, 1.30-3pm",
 		fee: "2",
 		size: 20
@@ -43,8 +42,8 @@ module.exports = {
 		title: "Henna Painting",
 		splash: "/img/headers/Workshop_NamasteHenna.jpg",
 		description: "Ever wanted to get an awesome tattoo that doesn’t stay on your skin for the rest of your life? Or the chance to draw on your friends without them throwing looks of disdain in your direction? Henna painting has been a long-practiced form of body art, famous for its intricate and beautiful patterns. Come by the workshop to try your hand(s) at henna painting!", 
-		venue: "Think Tank TBC",
-		timing: "October 11th, 5.30-7.30pm",
+		venue: "Think Tank 18",
+		timing: "October 12th, 4.30-6.30pm",
 		fee: "5 (includes papers and henna cone)",
 		size: 30
 	},
@@ -53,22 +52,15 @@ module.exports = {
 		splash: "/img/headers/Workshop_NamasteRangoli.jpg",
 		description: "Rangoli is an art form from India in which patterns are created on the floor in living rooms or courtyards using materials such as colored rice, dry flour, colored sand or flower petals. While mainly used as a form of decor, it is also thought to bring good luck with the use of auspicious symbols central to the design. It’s a great way to decorate your cohort classes, especially right before exams. \n(Disclaimer: does not guarantee winning cleanest class award.)", 
 		venue: "Campus Center",
-		timing: "October 13th, 5-7pm",
+		timing: "October 12th, 6-8pm",
 		fee: "5 (includes Rangoli powder)",
 		size: 10
 	},	
-	"introductiontodrama": {
-		title: "Introduction to Drama",
-		splash: "/img/headers/Workshop_Drama.jpg",
-		description: "If you think drama is all about acting, then you are wrong! Join us for a session, we will redefine fun/mindf*cked for you ! ;)", 
-		venue: "TBC",
-		timing: "TBC"
-	},
 	"bollywoodjazz": {
 		title: "Bollywood Jazz",
 		splash: "/img/headers/Workshop_Taal.jpg",
 		description: "Coming a long way from dances around coconut trees, Bollywood Jazz emerged as a result of the diverse and ever-changing trends in the Indian dance industry. Kick off your shoes for a fast-paced fusion of Indian music and contemporary Western dance, and shimmy the stress away.", 
-		venue: "Dance Studio TBC",
+		venue: "Dance Studio 8",
 		timing: "October 13th, 7.30-9.30pm",
 		size: 20
 	},
@@ -76,58 +68,48 @@ module.exports = {
 		title: "Hip-hop",
 		splash: "/img/headers/Workshop_Ddz.jpg",
 		description: "Introduction to Hip Hop workshop conducted by our very own instructor Marcus Leong! Open to all those interested in trying out dance, or those who are just curious as to what hip hop entails. Or even those who just want to understand why your friends are so crazy about dance.", 
-		venue: "TBC",
-		timing: "TBC",
+		venue: "Dance Studio 1 & 2",
+		timing: "October 12th, 7.30-9.30pm",
 		fee: "3"
 	},	
 	"introductiontocontemporarydance": {
 		title: "Introduction to Contemporary Dance",
 		splash: "/img/headers/Workshop_Fluence.jpg",
 		description: "Inspired by classical ballet and modern dance techniques, contemporary dance has become one of the most practiced genres of dance today. This workshop covers basic techniques such as floor work and body awareness, while introducing participants to the practice of creative physical expression.", 
-		venue: "Campus Center",
-		timing: "October 11th, 5.40-7pm",
+		venue: "Dance Studio 1 & 2",
+		timing: "October 11th, 5.30-7pm",
 		fee: "5",
 		size: 20
 	},	
-	"funkstyle": {
-		title: "Funkstyle",
-		splash: "/img/headers/Workshop_Funkstyle.jpg",
-		description: "The Funkstyle community in Singapore has grown into a vibrant scene, with names such as Lion City Lockers alongside various Funkstyle dance clubs from NUS, NTU and other universities. Funkstyle Workshop by our very own Dance Funktion dF(x) crew will show you how to pop it and lock it - like you really mean it.", 
-		venue: "TBC",
-		timing: "TBC",
-		fee: "5"
-	}, 
 	"waacking": {
 		title: "Waacking", 
 		splash: "/img/headers/Workshop_Waacking.jpg", 
 		description: "Ever seen those crazy arms in Facebook videos? Or simply have a love for disco? Waack it away at Xiang Yi's Waacking Workshop!",
-		venue: "TBC", 
-		timing: "TBC",
+		venue: "Dance Studio 1 & 2", 
+		timing: "October 14th, 5.30-6.30pm",
 		fee: "5",
 		size: "20-30"
-	}, 
-	"streetjazz": {
-		title: "Street Jazz", 
-		splash:"/img/headers/Workshop_StreetJazz.jpg", 
-		description: "Wanna get your inner diva out and get sassy? Take it away with Ben Chia's Street Jazz Workshop! ", 
-		venue: "TBC", 
-		timing: "TBC", 
-		fee: "5", 
-		size: "20-30" 
 	}, 
 	"batikpainting": {
 		title: "Batik Painting", 
 		splash: "/img/headers/Workshop_PADI.jpg", 
 		description: "",
-		venue: "TBC", 
-		timing: "TBC", 
+		venue: "Outside LT1", 
+		timing: "October 12th, 4.30-6.30pm", 
 		fee: "10"
 	}, 
+	"saman": {
+		title: "Saman Dance", 
+		splash: "/img/headers/Woekshop_PADI.jpg",
+		description: "Also called ‘the dance of thousand hands’, the Saman dance is one of the most practiced among Indonesian traditional dances. Characterized by its fast-paced rhythm and common harmony between dancers, these two elements are key figures of Saman, along with its notable disuse of music instruments. Audience members are invited to clap along to the dance, joining the performance from the crowd.", 
+		venue: "Space Bar", 
+		timing: "14th October, 3.30-5.30pm", 
+	},	
 	"doodling": {
 		title: "Doodling", 
 		splash: "/img/headers/Workshop_Multiverse.jpg", 
 		description: "", 
-		venue: "TBC", 
-		timing: "TBC"
+		venue: "Campus Center", 
+		timing: "October 11th, 7-9pm"
 	}
 }
